e2e97b1d7658638a69a7d4d847e2868b2a5ff85d
FrankXiong <Frank_Xiong_@outlook.com>
2019-12-27 09:40:10 +0800

    update gcc compile script

--- projects/GCC/release.sh
+++ projects/GCC/release.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
 
-source ~/.bash_profile
+cd $(dirname "$0")
 
-PROJECT_NAME=SourceCounter
-PROJECT_DIR=../..
-SOURCE_DIR=${PROJECT_DIR}/sources
-OUTPUT_DIR=${PROJECT_DIR}/outputs
-
-if [ ! -d ${OUTPUT_DIR}  ]; then
-  mkdir ${OUTPUT_DIR}
-fi
-
-# g++ -c ${SOURCE_DIR}/config/LangRules.cpp -o ${OUTPUT_DIR}/LangRules.o --std=c++17
-# g++ -c ${SOURCE_DIR}/config/Option.cpp --std=c++17
-# g++ -c ${SOURCE_DIR}/counter/analyzer/Analyzer.cpp --std=c++17
-# g++ -c ${SOURCE_DIR}/counter/Rapporteur.cpp --std=c++17
-`g++` -c ../../sources/Main.cpp --std=c++17
+./compile-script.sh -O3
